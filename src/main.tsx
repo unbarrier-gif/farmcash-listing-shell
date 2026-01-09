aeba025b2b77b366b8757b20594de9e842fd8f6d
unbarrier-gif <nici@unbarrier.me>
2026-01-09 15:48:12 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Could not find root element to mount to");
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
