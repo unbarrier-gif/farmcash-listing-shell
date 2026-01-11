9db66a2ad3ea0d823567c2865b06406044d4b7ef
unbarrier-gif <nici@unbarrier.me>
2026-01-11 20:10:44 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,29 @@ const App: React.FC = () => {
     <Routes>
       <Route element={<PortalLayout />}>
         <Route path="/" element={<Home mode="all" />} />
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import PortalLayout from "./layouts/PortalLayout";
+
+import Home from "./pages/Home";
+import Wanted from "./pages/Wanted";
+import Listing from "./pages/Listing";
+
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route element={<PortalLayout />}>
+        <Route path="/" element={<Home />} />
         <Route path="/for-sale" element={<Home mode="forSale" />} />
+        <Route path="/wanted" element={<Wanted />} />
+        <Route path="/listing/:id" element={<Listing />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
         <Route path="/wanted" element={<Wanted />} />
         <Route path="/listing/:id" element={<Listing />} />
         <Route path="*" element={<Navigate to="/" replace />} />
