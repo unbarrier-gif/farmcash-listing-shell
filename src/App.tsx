ec683f0785a7b1908fa107fca122ee66b39416df
unbarrier-gif <nici@unbarrier.me>
2026-01-11 15:23:38 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import PortalLayout from "./layouts/PortalLayout";
 
 import Home from "./pages/Home";
-import Listing from "./pages/Listing";
+import ForSale from "./pages/ForSale";
 import Wanted from "./pages/Wanted";
-import PortalHeader from "./components/PortalHeader";
-import PortalFooter from "./components/PortalFooter";
+import Listing from "./pages/Listing";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-50 text-gray-900 flex flex-col">
-      <PortalHeader />
-
-      <main className="flex-1">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/for-sale" element={<Home />} />
-            <Route path="/listing/:slug" element={<Listing />} />
-            <Route path="/wanted" element={<Wanted />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
-      </main>
-
-      <PortalFooter />
-    </div>
+    <Routes>
+      <Route element={<PortalLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/for-sale" element={<ForSale />} />
+        <Route path="/wanted" element={<Wanted />} />
+        <Route path="/listing/:id" element={<Listing />} />
+      </Route>
+    </Routes>
   );
 };
 
