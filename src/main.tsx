7e8d456ba79a3c020e72654f6e4baa897a7de2fc
unbarrier-gif <nici@unbarrier.me>
2026-01-11 13:58:07 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css"; // 🔴 THIS LINE IS REQUIRED
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Could not find root element to mount to");
