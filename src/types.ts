d3c967702f8927d18ea8831992894644190e4221
unbarrier-gif <nici@unbarrier.me>
2026-01-12 20:00:40 +0000

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,4 @@
-
 export interface ListingImageData {
   url: string;
   alt: string;
-}
-
-// AspectRatio options supported by the Veo video generation API
-export enum AspectRatio {
-  LANDSCAPE = '16:9',
-  PORTRAIT = '9:16'
-}
-
 }
